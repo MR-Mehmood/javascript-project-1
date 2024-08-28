ff9f38bc4205747324e92a20214c08f742b365bf
MR-Mehmood <160325537+MR-Mehmood@users.noreply.github.com>
2024-08-28 06:24:09 +0500

    // section-numbar-2
    // section-numbar-2

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-// section-numbar-3
+// section-numbar-2
+
+
 
 // Q-num-1
-var age = 15; 
-alert("I am " +age+ " years old");
+var username;
 
 // Q-num-2
-var visits = 15;
-alert("You have visited this site " + visits + " times.");
+var myName = "MEHMOOD JAMIL"; 
+alert("Full Name:" + myName);
 
 // Q-num-3
-var birthYear = 2005; 
-document.write("My birth year is " + birthYear + ".<br>");
-document.write("I am " + (2024 - birthYear) + " years old. <br>");
-
+var message;
+message = "Hello World";
+alert(message);
 
 // Q-num-4
-var visitorName = "Kifal Hamad";
-var productTitle = "T-shirt";
-var quantity = 10;
+var studentName = "MEHMOOD JAMIL"; 
+var studentAge = 18; 
+var studentGrade = "A"; 
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Grade: " + studentGrade);
 
-document.write(visitorName + " ordered " + quantity + " " + productTitle + "(M) on alxzanda Clothing store.");
+// Q-num-5
+var alertMessage = "pizza \n pizz \n piz \n pi \n p";
+alert(alertMessage);
 
+// Q-num-6
+var email = "balochsami479@gmail.com"; 
+alert("My email address is: " + email);
 
+// Q-num-7
+var book = "A English way to learn english language";
+alert("My favorite book is: " + book);
